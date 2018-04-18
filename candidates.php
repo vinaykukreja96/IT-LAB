6bcd9c3fbb14d42eea454b918006c95b35aecf07
vinaykukreja96 <38484769+vinaykukreja96@users.noreply.github.com>
2018-04-18 09:55:36 +0530

    Create candidates.php

---
+++ candidates.php
@@ -0,0 +1,13 @@
+<?php
+	echo '<form id="myform">';
+	echo '<input type="radio" id="name1" name="candidate" value="ABC">ABC</input><br />';
+	echo '<input type="radio" id="name1" name="candidate" value="XYZ">XYZ</input><br />';
+	echo '<input type="radio" id="name1"  name="candidate" value="PQR">PQR</input><br />';
+	echo '</form>';
+?>
+
+
+
+
+
+ 
